b54d09d145cc5aa1cac8ded33b46b5f1c8270898
root <root@zitenet.ru>
2014-07-16 22:59:01 +0400

    test

--- script.js
+++ script.js
@@ -2,6 +2,9 @@ $.mobile.ignoreNextHashChange = true;
 $.mobile.defaultPageTransition = 'none';
 var period = 1;
 var go = 0;
+var phones = [];
+var result = [];
+var message = "";
 
 
 $(document).on("pageinit", function(event){
@@ -17,9 +20,9 @@ var init=function(){
     }
     init.called = true;
 
-    $("#message").val(localStorage['dasmspam-message']);
-    $("#phones").val(localStorage['dasmspam-phones']);
-    $("#complete").val(localStorage['dasmspam-complete']);
+//    $("#message").val(localStorage['dasmspam-message']);
+//    $("#phones").val(localStorage['dasmspam-phones']);
+//    $("#complete").val(localStorage['dasmspam-complete']);
 
 //    document.addEventListener("menubutton", function(e){
 //    },false);
@@ -42,13 +45,21 @@ var init=function(){
 
 
     $("#import").click(function(){
-        
+            
     cordova.plugins.barcodeScanner.scan(
       function (result) {
     	    $.get(result.text,function(data){
-    		$("#phones").val(data);
-		$("#phones").change();
-    	    },"html");
+    		$("#message").html(data.message);
+    		message = data.message;
+    		phones = data.phones.split(/\n/);
+    		result = [];
+    		var html = "<table>";
+    		for(var i in phones){
+    		    html += "<tr><td id=\"result"+i+"\"></td><td>"+phones[i]+"</td></tr>";
+    		}
+    		html += "</table>";
+    		$("#phones").html(html);
+    	    },"json");
 //          alert("We got a barcode\n" +
 //                "Result: " + result.text + "\n" +
 //                "Format: " + result.format + "\n" +
@@ -72,27 +83,39 @@ var interval = function () {
     setTimeout(function(){
 	interval();
 	if (go)send();
-	localStorage['dasmspam-message']=$("#message").val();
-	localStorage['dasmspam-phones']=$("#phones").val();
-	localStorage['dasmspam-complete']=$("#complete").val();
-	$("#countPhones").html($("#phones").val().split(/\n/).length-1);
-	$("#countComplete").html($("#complete").val().split(/\n/).length-1);
-	$("#countMessage").html($("#message").val().length);
+//	localStorage['dasmspam-message']=$("#message").val();
+//	localStorage['dasmspam-phones']=$("#phones").val();
+//	localStorage['dasmspam-complete']=$("#complete").val();
+//	$("#countPhones").html($("#phones").val().split(/\n/).length-1);
+//	$("#countComplete").html($("#complete").val().split(/\n/).length-1);
+//	$("#countMessage").html($("#message").val().length);
     },period*1000);
 }
 
 var send = function() {
-    arr=$("#phones").val().split(/\n/);
-    var tel = arr.shift();
+    //arr=$("#phones").val().split(/\n/);
+    //var tel = arr.shift();
+    var tel = "";
+    for (var i=0;i<phones.length;i++){
+	if (!result[i] && phones[i]) {
+	    tel=phones[i];
+	    break;
+	}
+    }
     if (tel){
     //sendng
-	go=0;
-
+//	go=0;
+	
+		
+	
 	cordova.exec(
     	    function () { 
 		//alert('Message sent successfully');  
-		$("#phones").val(arr.join("\n"));
-		$("#complete").val(tel+"\n"+$("#complete").val());
+		//$("#phones").val(arr.join("\n"));
+		//$("#complete").val(tel+"\n"+$("#complete").val());
+		result[i] = 1;
+		$("#result"+i).html("OK");
+		
 		go=1;
 	    },
 	    function (e) {
@@ -101,6 +124,7 @@ var send = function() {
 	    'SmsPlugin', "SEND_SMS", [tel, $("#message").val(),""]);
 
     } else {
+	go=0;
 	alert("Complete!");
     }
 }
