7d5beb986546a01b5132cdce81682d90e8829e40
Polyakov <polyakov@premium-a.ru>
2019-03-11 12:31:42 +0300

    test36

--- script.js
+++ script.js
@@ -141,12 +141,12 @@ var send = function() {
         var options = {
             replaceLineBreaks: false, // true to replace \n by a new line, false by default
             android: {
-                //intent: 'INTENT' // send SMS with the native android SMS messaging
-                intent: '' // send SMS without opening any other app
+                intent: 'INTENT' // send SMS with the native android SMS messaging
+  //              intent: '' // send SMS without opening any other app
             }
         };
 
-alert(tel);
+//alert(tel);
         sms.send(tel, message, options,
             function() {
                 alert("ok");
